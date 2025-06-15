ea829fadde8012a3b7d8e8ab4f88407e9d4c0803
Jones <samamedejones@gmail.com>
2025-06-15 20:01:38 -0300

    schemas padrão

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import GitHub from "next-auth/providers/github"
+import { prisma } from "./prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 })
