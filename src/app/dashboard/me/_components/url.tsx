c831f28f72b78b7c5b8e96b72492ab15762f3734
Jones <samamedejones@gmail.com>
2025-06-19 21:35:26 -0300

    estilização button + input do perfil

--- src/app/dashboard/me/_components/url.tsx
+++ src/app/dashboard/me/_components/url.tsx
@@ -4,19 +4,24 @@ import { Button } from "@/components/ui/button";
 export function UrlPreview() {
   return (
     <div className="flex items-center flex-1 p-2 text-gray-100">
-        <form className="flex-1 flex flex-col"> 
+        <form className="flex-1 flex flex-col md:flex-row gap-4 items-start md:items-center"> 
         <div className="flex items-center justify-center w-full">
         <p>
             {process.env.NEXT_PUBLIC_HOST_URL}/creator/
         </p>
         <input
           type="text"
-          className="flex-1 outline-none border h-9 border-gray-300 text-black rounded-md"
+          className="flex-1 outline-none border h-9 border-gray-300 text-black rounded-md bg-gray-50 px-1"
+          placeholder="Digite seu nome de usuário"
+          name="username"
         />
         </div>
 
-        <Button type="submit">
-          Create URL
+        <Button 
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 h-9 w-full md:w-fit text-white px-4 rounded-md cursor-pointer"
+        >
+          Salvar
         </Button>
     </form>
     </div>
