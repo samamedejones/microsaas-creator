b104365e4a2d7139a4e88c6ec535ee127853e65c
Jones <samamedejones@gmail.com>
2025-06-20 19:33:03 -0300

    validação do campo username

--- src/app/dashboard/me/_actions/create_username.tsx
+++ src/app/dashboard/me/_actions/create_username.tsx
@@ -1,5 +1,13 @@
 "user server"
 
-export async function createUsername(username: string) {
-  
+import { z } from "zod";
+
+const createUsernameSchema = z.object({
+    username: z.string( { message: "o username é obrigatório" }).min(3, "Username precisa ter pelo menos 4 caracteres")
+})
+
+type CreateUsernameFormData = z.infer<typeof createUsernameSchema>;
+
+export async function creatUsername(data: CreateUsernameFormData){
+
 }
