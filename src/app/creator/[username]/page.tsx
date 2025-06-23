88e531dc421eb49aa011fa30a86d567e60c84450
Jones <samamedejones@gmail.com>
2025-06-23 14:40:14 -0300

    data action de creator username

--- src/app/creator/[username]/page.tsx
+++ src/app/creator/[username]/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { getInfoUser } from "./_data-acess/get-info-user";
+import { notFound } from "next/navigation";
 
 export default async function Apoia({
   params,
@@ -7,8 +9,11 @@ export default async function Apoia({
 }) {
   const { username } = await params;
 
+  const user = await getInfoUser({ username });
 
-  console.log(username);
+   if (!user) {
+    notFound();
+   }
 
   return (
     <div className=" min-h-[calc(100vh-64px)]">
@@ -26,7 +31,7 @@ export default async function Apoia({
       <section className="flex flex-col w-full items-center justify-center mx-auto max-w-7xl p-4 relative">
         <div className="flex flex-col items-center">
           <Image
-            src={"https://github.com/devfraga.png"}
+            src={user.image ?? "https://github.com/devfraga.png"}
             className="w-36 h-36 rounded-xl bg-gray-50 hover:shadow-lg duration-300 select-none text-zinc-900 text-3xl flex items-center justify-center object-cover absolute -top-16 border-4 border-white"
             alt="Matheus Fraga"
             width={96}
@@ -34,7 +39,7 @@ export default async function Apoia({
             quality={100}
           />
           <h1 className=" font-bold text-xl md:text-2xl mt-20 mb-4">
-            Fulano Dev
+            {user.name ?? "Usuario sem nome"}
           </h1>
         </div>
       </section>
@@ -42,10 +47,10 @@ export default async function Apoia({
       <div className="grid grid-cols-1 md:grid-cols-2 w-full mx-auto gap-4 max-w-5xl">
         <section className="hidden md:flex flex-col bg-gray-50 p-5 rounded-md h-fit mx-2">
           <p className="font-semibold text-lg">
-            Sobre Fulano Dev
+            {"Sobre " + user.name}
           </p>
           <p className="text-gray-500 mt-2">
-            Descrição generica sobre o fulano dev
+            {user.bio ?? "Este usuário não possui uma biografia definida."}
           </p>
         </section>
 
