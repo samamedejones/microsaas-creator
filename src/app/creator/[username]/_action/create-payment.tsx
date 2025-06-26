1ba19235fc1629a2156c96cf33834e49fca4de3d
Jones <samamedejones@gmail.com>
2025-06-26 14:32:26 -0300

    função criar donate

--- src/app/creator/[username]/_action/create-payment.tsx
+++ src/app/creator/[username]/_action/create-payment.tsx
@@ -7,8 +7,8 @@ const createPaymentSchema = z.object({
     slug: z.string().min(1, "A slug do criador é obrigatório"),
     name: z.string().min(1, "O name precisa ser preenchido"),
     message: z.string().min(1, "A mensagem é obrigatória"),
-    price: z.number().min(1500, "O valor mínimo é R$15,00"),
-    creatorId: z.string()
+    price: z.number().min(500, "O valor mínimo é R$5,00"),
+    creatorId: z.string(),
 });
 
 type CreatePaymentSchema = z.infer<typeof createPaymentSchema>;
@@ -25,20 +25,43 @@ export async function CreatePayment(data: CreatePaymentSchema) {
         }
     }
 
+    if(!data.creatorId){
+         return {
+            data: null,
+            error: "Creator não encontrado"
+    }
+}
+
+
+
     try {
         
-        const creator = await prisma.user.findUnique({
+        const creator = await prisma.user.findFirst({
             where: {
-                id: data.creatorId
+                connectedStripeAccountId: data.creatorId
             }
         })
 
         if(!creator){
             return {
             data: null,
-            err: "Falha ao criar pagamento, tente novamente mais tarde."
+            err: "Falha ao criar pagamento, tente novamente mais tarde"
+            }
         }
-        }
+
+        const aplicationFeeAmount = Math.floor(data.price * 0.1)
+
+        const donation = await prisma.donation.create({
+            data: {
+                donorName: data.name,
+                donorMessage: data.message,
+                userId: creator.id,
+                status: "PENDING",
+                amount: (data.price - aplicationFeeAmount)
+            }
+        })
+
+         
 
     } catch (err) {
         return {
